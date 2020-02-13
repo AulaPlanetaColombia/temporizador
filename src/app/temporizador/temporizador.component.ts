9bda9f9a58179cbf6727d76a9497dbd27ead7976
Oliver Castelblanco Martínez <ocastelblanco@gmail.com>
2020-02-13 17:09:51 -0500

    add crono

--- src/app/temporizador/temporizador.component.ts
+++ src/app/temporizador/temporizador.component.ts
@@ -17,14 +17,19 @@ export class TemporizadorComponent implements OnInit, OnChanges {
     paso:number = 0;
     conteo:number;
     crono:Boolean;
+    salida:string;
+    safari:Boolean;
     constructor(private tiempoFuera:MatSnackBar) {}
     ngOnInit() {
         this.conteo = this.tiempo;
         this.calculaTiempo();
+        this.salida = String(this.minutos) + ' : ' + String(this.segundos);
+        this.tipoBrowser();
     }
     ngOnChanges(changes:SimpleChanges) {
         this.conteo = this.tiempo;
         this.calculaTiempo();
+        this.salida = String(this.minutos) + ' : ' + String(this.segundos);
     }
     calculaTiempo() {
         if (this.tiempo === 0) {
@@ -38,11 +43,15 @@ export class TemporizadorComponent implements OnInit, OnChanges {
         this.paso = 1;
         const fuente = timer(0,1000);
         this.sec = fuente.subscribe(mil => {
-            if (this.conteo === 0 && !this.crono) {
+            if (this.conteo === 0) {
                 this.tiempoFuera.open('¡Tiempo fuera! ⏰','Cerrar',{duration:5000});
             }
             this.calculaTiempo();
-            this.crono ? this.conteo++ : this.conteo--;
+            this.salida = '';
+            window.setTimeout(()=>{
+                this.salida = String(this.minutos) + ' : ' + String(this.segundos);
+            });
+            this.conteo--;
         });
     }
     pausa() {
@@ -60,6 +69,20 @@ export class TemporizadorComponent implements OnInit, OnChanges {
             return '0' + String(num);
         } else {
             return String(num);
+        }
+    }
+    tipoBrowser() {
+        let chrome:Boolean = navigator.userAgent.indexOf('Chrome') > -1;
+        let ie:Boolean = navigator.userAgent.indexOf('MSIE') > -1 || navigator.userAgent.indexOf('rv:') > -1;
+        let firefox:Boolean = navigator.userAgent.indexOf('Firefox') > -1;
+        let safari:Boolean = navigator.userAgent.indexOf('Safari') > -1;
+        if ((chrome) && (safari)) safari = false;
+        let opera:Boolean = navigator.userAgent.indexOf('OP') > -1;
+        if ((chrome) && (opera)) chrome = false;
+        if (safari) {
+            this.safari = true;
+        } else {
+            this.safari = false;
         }
     }
 }
